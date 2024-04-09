e720e446b1770e2a28b9315860e30c763ef6c3d4
cqb13 <cqb13.main@gmail.com>
2024-04-09 10:36:18 -0400

    calculating current point for a_star

--- src/algorithms/a_star.rs
+++ src/algorithms/a_star.rs
@@ -4,7 +4,7 @@ use super::Point;
 use crate::algorithms::GridMap;
 
 pub fn run(grid: &GridMap, start: &Point, end: &Point) {
-    let mut open_set = vec![(start, 0)];
+    let mut open_set = vec![start];
     let mut came_from: Vec<&Point> = Vec::new();
 
     let mut gscore: Vec<(Point, i32)> = Vec::new();
@@ -21,7 +21,31 @@ pub fn run(grid: &GridMap, start: &Point, end: &Point) {
     gscore.push((Point::new(start.x, start.y), 0));
 
     let mut fscore = gscore.clone();
-    fscore.push((Point::new(start.x, start.y), heuristic(start, end)))
+    fscore.push((Point::new(start.x, start.y), heuristic(start, end)));
+
+    while !open_set.is_empty() {
+        let current = calculate_current_node(&open_set, &fscore);
+        if current.0 == *end {
+            println!("Found the goal")
+        }
+
+        
+    }
+}
+
+// node in open_set with lowest fscore value
+fn calculate_current_node(open_set: &Vec<&Point>, fscore: &Vec<(Point, i32)>) -> (Point, i32){
+    let mut current = open_set[0];
+    let mut current_fscore = fscore[0].1;
+
+    for (i, node) in open_set.iter().enumerate() {
+        if fscore[i].1 < current_fscore {
+            current = node;
+            current_fscore = fscore[i].1;
+        }
+    }
+
+    return (current.clone(), current_fscore);
 }
 
 fn reconstruct_path(came_from: Vec<&Point>, current: (&Point, i32)) {}
